e7ba0af6d16e817046ef107812578f399cd3ae0f
Patrick Double <pat@patdouble.com>
2023-07-10 05:41:45 -0500

    wip subtitle alignment: fix overlaps, changed flag reset

--- dvrprocess/test_subtitle_alignment.py
+++ dvrprocess/test_subtitle_alignment.py
@@ -4,6 +4,7 @@ import re
 import tempfile
 import unittest
 from pathlib import Path
+from typing import Union
 
 import pysrt
 import pytest
@@ -24,114 +25,99 @@ class SubtitleAlignmentTest(unittest.TestCase):
     @pytest.fixture(autouse=True)
     def inject_fixtures(self, caplog):
         self._caplog = caplog
+        self._caplog.set_level(logging.DEBUG)
 
-    def _read_aligned_and_original_ass(self, aligned_filename: str, original_filename: str) -> (AssFile, AssFile):
-        """
-        Read the expected alignment. This is special because only event with start and end that have an SRT like
-        timestamp will be used.
-        :return: aligned, original
-        """
-        suffix = original_filename.split('.')[-1]
-        aligned_fd, filtered_aligned_path = tempfile.mkstemp(suffix=suffix)
-        os.close(aligned_fd)
-        original_fd, filtered_original_path = tempfile.mkstemp(suffix=suffix)
-        os.close(original_fd)
-
-        aligned = subtitle.open_subtitle_file_facade(Path(aligned_filename))
-        aligned_dict = dict()
-        for event_idx, event in aligned.events():
-            aligned_dict[event.text().__hash__()] = event
-        aligned.write(Path(filtered_aligned_path))
-
-        original = subtitle.open_subtitle_file_facade(Path(original_filename))
-        original_dict = dict()
-        for event_idx, event in reversed(list(original.events())):
-            if event.text().__hash__() not in aligned_dict:
-
-                original_dict[event.text().__hash__()] = event
-        original.write(Path(filtered_original_path))
-
-        aligned_subtitle = read_ass(Path(filtered_aligned_path))
-        original_subtitle = read_ass(Path(filtered_original_path))
-        os.remove(filtered_aligned_path)
-        os.remove(filtered_original_path)
-        return aligned, original_subtitle
+    def _read_subtitle(self, filename: str) -> Union[AssFile, SubRipFile]:
+        p = Path(f"../fixtures/{filename}")
+        if filename.endswith('.srt'):
+            return pysrt.open(p)
+        else:
+            return read_ass(p)
 
     def _read_words_srt(self, filename: str) -> SubRipFile:
         subtitle = pysrt.open(f"../fixtures/{filename}")
         return subtitle
 
     def _assert_alignment(self, aligned_filename: str, original_filename: str, words_filename: str):
-        expected_alignment, original = self._read_aligned_and_original_ass(aligned_filename,
-                                                                           original_filename)
+        expected_alignment = subtitle.open_subtitle_file_facade(Path(f"../fixtures/{aligned_filename}"))
+        expected_alignment_events = list(map(lambda e: e[1], expected_alignment.events()))
+        original = subtitle.open_subtitle_file_facade(Path(f"../fixtures/{original_filename}"))
         words = self._read_words_srt(words_filename)
-        changed = profanity_filter.fix_subtitle_audio_alignment(original, words)
+        changed = profanity_filter.fix_subtitle_audio_alignment(original.file, words)
         failed = []
-        for idx, actual_event in enumerate(original.events):
-            expected_event = expected_alignment.events[idx]
-            expected_start_diff = abs(expected_event.start - actual_event.start)
-            if expected_start_diff > 400:
+        original_count = 0
+        for idx, actual_event in original.events():
+            original_count += 1
+            if idx < len(expected_alignment_events):
+                expected_event = expected_alignment_events[idx]
+                expected_start_diff = abs(expected_event.start() - actual_event.start())
+                if actual_event.text() != expected_event.text():
+                    failed.append((
+                        s_to_ts(expected_event.start() / 1000),
+                        s_to_ts(actual_event.start() / 1000),
+                        expected_start_diff, f"Event mismatch {idx}: '{actual_event.text()}' != '{expected_event.text()}'")
+                    )
+                    break
+                if expected_start_diff > 400:
+                    failed.append((
+                        s_to_ts(expected_event.start() / 1000),
+                        s_to_ts(actual_event.start() / 1000),
+                        expected_start_diff, f"Start event {idx}: '{actual_event.text()}'")
+                    )
+                expected_end_diff = abs(expected_event.end() - actual_event.end())
+                if expected_end_diff > 400:
+                    failed.append((
+                        s_to_ts(expected_event.end() / 1000),
+                        s_to_ts(actual_event.end() / 1000),
+                        expected_end_diff, f"End event {idx}: '{actual_event.text()}'")
+                    )
+            else:
                 failed.append((
-                    s_to_ts(expected_event.start/1000),
-                    s_to_ts(actual_event.start/1000),
-                    expected_start_diff, f"Start event {idx}: '{actual_event.text}'")
-                )
-            expected_end_diff = abs(expected_event.end - actual_event.end)
-            if expected_end_diff > 400:
-                failed.append((
-                    s_to_ts(expected_event.end/1000),
-                    s_to_ts(actual_event.end/1000),
-                    expected_end_diff, f"End event {idx}: '{actual_event.text}'")
+                    0,
+                    s_to_ts(actual_event.start() / 1000),
+                    actual_event.start(), f"Missing event {idx}: '{actual_event.text()}'")
                 )
         failed.sort(key=lambda e: e[2], reverse=True)
-        self.assertEqual(0, len(failed), str(failed) + "\n" + str(len(failed)) + "/" + str(len(original.events)*2))
+
+        actual_fd, actual_path = tempfile.mkstemp(prefix=aligned_filename.split('aligned.')[0],
+                                                  suffix='.actual-aligned.' + aligned_filename.split('aligned.')[1])
+        os.close(actual_fd)
+        original.write(Path(actual_path))
+        log_path = actual_path.replace('.ssa', '.log').replace('.srt', '.log')
+        with open(log_path, 'w') as f:
+            f.write(self._caplog.text)
+        print(f"Wrote actual aligned file to {actual_path}, log to {log_path}")
+
+        self.assertEqual(0, len(failed), str(failed) + "\n" + str(len(failed)) + "/" + str(original_count * 2))
         # self.assertEqual(0, len(failed), str(len(failed)) + "/" + str(len(original.events)*2))
         self.assertEqual(True, changed, 'fix_subtitle_audio_alignment should have returned changed')
+
+    def _assert_idempotent(self, original_filename: str, words_filename: str):
+        original = self._read_subtitle(original_filename)
+        words = self._read_words_srt(words_filename)
+        changed1 = profanity_filter.fix_subtitle_audio_alignment(original, words)
+        changed2 = profanity_filter.fix_subtitle_audio_alignment(original, words)
+        log_fd, log_path = tempfile.mkstemp(prefix=original_filename.split('-original.')[0]+'-aligned.', suffix='.log')
+        os.write(log_fd, self._caplog.text.encode('utf-8'))
+        os.close(log_fd)
+        print(f'idempotent log at {log_path}')
+        self.assertEqual(True, changed1, 'fix_subtitle_audio_alignment should have returned changed for first run')
+        self.assertEqual(False, changed2, 'fix_subtitle_audio_alignment should have returned unchanged for second run')
 
     def test_bones_s02e11(self):
         self._assert_alignment('bones-s02e11-aligned.ssa', 'bones-s02e11-original.ssa', 'bones-s02e11-words.srt')
 
-    def test_idempotent(self):
-        expected_alignment, original = self._read_aligned_and_original_ass('bones-s02e11-aligned.ssa', 'bones-s02e11-original.ssa')
-        words = self._read_words_srt('bones-s02e11-words.srt')
-        changed1 = profanity_filter.fix_subtitle_audio_alignment(original, words)
-        changed2 = profanity_filter.fix_subtitle_audio_alignment(original, words)
-        self.assertEqual(True, changed1, 'fix_subtitle_audio_alignment should have returned changed for first run')
-        self.assertEqual(False, changed2, 'fix_subtitle_audio_alignment should have returned unchanged for second run')
+    def test_idempotent_bones_s02e11(self):
+        self._assert_idempotent('bones-s02e11-original.ssa', 'bones-s02e11-words.srt')
 
     def test_house_s03e07(self):
         self._assert_alignment('house-s03e07-aligned.ssa', 'house-s03e07-original.ssa', 'house-s03e07-words.srt')
 
+    def test_idempotent_house_s03e07(self):
+        self._assert_idempotent('house-s03e07-original.ssa', 'house-s03e07-words.srt')
+
     def test_meninblack(self):
-        self._caplog.set_level(logging.DEBUG)
         self._assert_alignment('men-in-black-aligned.srt', 'men-in-black-original.srt', 'men-in-black-words.srt')
 
-    def test_house_s03e07_output(self):
-        original = read_ass(Path('../fixtures/house-s03e07-original.ssa'))
-        words = self._read_words_srt('house-s03e07-words.srt')
-        profanity_filter.fix_subtitle_audio_alignment(original, words)
-        aligned_fd, aligned_path = tempfile.mkstemp(prefix='house-s03e07-aligned', suffix='.ssa')
-        os.close(aligned_fd)
-        write_ass(original, Path(aligned_path))
-        print(f'house-s03e07 alignment in {aligned_path}')
-
-    def test_bones_s02e11_output(self):
-        original = read_ass(Path('../fixtures/bones-s02e11-original.ssa'))
-        words = self._read_words_srt('bones-s02e11-words.srt')
-        profanity_filter.fix_subtitle_audio_alignment(original, words)
-        aligned_fd, aligned_path = tempfile.mkstemp(prefix='bones-s02e11-aligned', suffix='.ssa')
-        os.close(aligned_fd)
-        write_ass(original, Path(aligned_path))
-        print(f'bones-s02e11 alignment in {aligned_path}')
-
-    def test_meninblack_output(self):
-        self._caplog.set_level(logging.DEBUG)
-        original = pysrt.open(Path('../fixtures/men-in-black-original.srt'))
-        words = self._read_words_srt('men-in-black-words.srt')
-        profanity_filter.fix_subtitle_audio_alignment(original, words)
-        aligned_fd, aligned_path = tempfile.mkstemp(prefix='men-in-black-aligned', suffix='.srt')
-        os.close(aligned_fd)
-        original.save(Path(aligned_path), 'utf-8')
-        with open(aligned_path.replace('.srt', '.log'), 'w') as f:
-            f.write(self._caplog.text)
-        print(f'men-in-black alignment in {aligned_path}')
+    def test_idempotent_meninblack(self):
+        self._assert_idempotent('men-in-black-original.srt', 'men-in-black-words.srt')
