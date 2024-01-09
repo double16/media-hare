122a5723279a56df075fc0681c49a0f1e652b12f
Patrick Double <pat@patdouble.com>
2024-01-09 15:59:34 -0600

    daily pf run ignores compute limits

--- profanity-filter-apply.sh
+++ profanity-filter-apply.sh
@@ -3,4 +3,4 @@
 # random sleep to prevent multiple programs running at the exact same time
 sleep $((5 + RANDOM % 30))
 
-exec ionice -c 3 nice -n 15 /usr/local/bin/profanity-filter-apply.py -u . "$@" >>/var/log/profanity-filter.log 2>&1
+exec ionice -c 3 nice -n 15 /usr/local/bin/profanity-filter-apply.py --ignore-compute -u . "$@" >>/var/log/profanity-filter.log 2>&1
