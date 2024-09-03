84cedea0ec20071391335439e569d2f5c69d143e
Patrick Double <pat@patdouble.com>
2024-09-03 15:31:28 -0500

    add tools to clean orphaned files and export music library

--- clean-orphaned-files.sh
+++ clean-orphaned-files.sh
@@ -3,4 +3,4 @@
 # random sleep to prevent multiple programs running at the exact same time
 sleep $((5 + RANDOM % 30))
 
-exec ionice -c 3 nice -n 15 /usr/local/bin/profanity-filter-apply.py --ignore-compute -u . "$@" >>/var/log/profanity-filter.log 2>&1
+exec ionice -c 3 nice -n 15 /usr/local/bin/clean_orphaned_files.py >>/var/log/clean_orphaned_files.log 2>&1
