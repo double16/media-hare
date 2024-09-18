b956264d3a6fad8e0c64494870bea34d56c2400f
Patrick Double <pat@patdouble.com>
2024-09-18 14:32:21 -0500

    Don't install gpustat

--- hwaccel-drivers.sh
+++ hwaccel-drivers.sh
@@ -22,20 +22,18 @@ if [ -s '/sys/module/nvidia/version' ]; then
       NV_PKG_VERSION="$(apt list -a "${NV_PKG}-${NV_MAJOR_VER}${NV_PKG_SUFFIX}" 2>/dev/null | grep -F "${NV_VERSION}" | head -n 1 | tr -s '[:space:]' | cut -d ' ' -f 2)"
       if [ -n "${NV_PKG_VERSION}" ]; then
         NV_PKG_INSTALL+=("${NV_PKG}-${NV_MAJOR_VER}${NV_PKG_SUFFIX}=${NV_PKG_VERSION}")
-      else
-        NV_PKG_INSTALL+=("${NV_PKG}-${NV_MAJOR_VER}${NV_PKG_SUFFIX}")
       fi
     fi
   done
-  for SUPPORT_PKG in gpustat; do
-    if ! apt list --installed "${SUPPORT_PKG}" 2>/dev/null | grep -q "${SUPPORT_PKG}"; then
-      if [[ -z "${APT_UPDATED}" ]]; then
-        apt-get update
-        APT_UPDATED=1
-      fi
-      NV_PKG_INSTALL+=("${SUPPORT_PKG}")
-    fi
-  done
+#  for SUPPORT_PKG in gpustat; do
+#    if ! apt list --installed "${SUPPORT_PKG}" 2>/dev/null | grep -q "${SUPPORT_PKG}"; then
+#      if [[ -z "${APT_UPDATED}" ]]; then
+#        apt-get update
+#        APT_UPDATED=1
+#      fi
+#      NV_PKG_INSTALL+=("${SUPPORT_PKG}")
+#    fi
+#  done
   if [ -n "${NV_PKG_INSTALL}" ]; then
       apt-get -y install "${NV_PKG_INSTALL[@]}"
   fi
