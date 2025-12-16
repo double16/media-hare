3b4cf3a9539c51d733611a69245c2fb4afa04fb6
Patrick Double <pat@patdouble.com>
2025-12-16 07:38:08 -0600

    Cache media errors.

--- media-errors.sh
+++ media-errors.sh
@@ -3,4 +3,4 @@
 # random sleep to prevent multiple programs running at the exact same time
 sleep $((5 + RANDOM % 30))
 
-exec ionice -c 3 nice -n 15 /usr/local/bin/find_media_errors.py "$@" >>/var/log/find_media_errors.log 2>&1
+exec ionice -c 3 nice -n 15 /usr/local/bin/find_media_errors.py --nagios "$@" | tee /var/cache/media_errors.txt >>/var/log/find_media_errors.log 2>&1
